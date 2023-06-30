3c0a269273146390b710619234516ac4d136ec8b
Rufus Ajiboye <ruufman@gmail.com>
2023-06-30 21:30:10 +0100

    Jenkinsfile updated to reflect staging

--- vars/helloWorld.groovy
+++
@@ -1,7 +0,0 @@
-def call() {
-    sh '''
-        cd codebase
-        terraform init -backend-config=../env/"${TARGET_ENV}"/backend.tfvars
-        terraform plan -var-file ../env/"${TARGET_ENV}"/backend.tfvars  -var-file ../env/"${TARGET_ENV}"/ec2.tfvars 
-        '''
-}
